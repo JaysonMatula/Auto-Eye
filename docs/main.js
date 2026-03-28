c41bcb98a4390c36c648ef0732b6d9e73eeed018
JaysonMatula <114369233+JaysonMatula@users.noreply.github.com>
2026-03-28 15:33:47 -0500

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -3,17 +3,18 @@ async function start() {
     const response = await fetch("https://vpic.nhtsa.dot.gov/api/vehicles/GetAllMakes?format=json");
     const data = await response.json();
 
+    /*
     const commonMakes = ["Acura", "Alfa Romeo", "Audi", "Bentley", "BMW", "Buick", "Cadillac", "Chevrolet", "Chrysler", "Dodge",
                          "Fiat", "Ford", "GMC", "Honda", "Hyundai", "Infiniti", "Jaguar", "Jeep", "Kia", "Land Rover", "Lexus",
                          "Lincoln", "Mazda", "Mercedes-Benz", "Mini", "Mitsubishi", "Nissan", "Polestar","Porsche", "Ram", "Rivian",
                          "Saab", "Subaru", "Suzuki", "Tesla", "Toyota", "Volkswagen", "Volvo"];
-                         
+    */         
 
   
     
     let makeOutput = `
-      ${commonMakes.map(function (make){
-        return `<option value="${make}">`;
+      ${data.Results.map(function (item){
+        return `<option value="${item.Make_Name}">`;
       }).join('')}`;
 
     document.getElementById("makes").innerHTML = makeOutput;
