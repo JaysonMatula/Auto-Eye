b5908e39285464b597b40857099ceef9c808de7a
JaysonMatula <114369233+JaysonMatula@users.noreply.github.com>
2026-04-13 11:08:28 -0500

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -199,6 +199,6 @@ function showVehicle() {
   output.innerHTML = `
     <h2>${query}</h2>
     <img src="${imageUrl}" alt="${query}" style="max-width:100%; border-radius:10px;"
-    onerror="this.src='https://via.placeholder.com/600x400?text=Car+Image+Not+Found'">
+    onerror="this.onerror=null; this.src='https://via.placeholder.com/600x400?text=No+Image';">
   `;
 }
